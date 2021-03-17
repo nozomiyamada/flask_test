bace60dc1265e37d9310ce367dfca4b2d5563651
Nozomi Yamada <44984892+nozomiyamada@users.noreply.github.com>
2021-03-17 12:44:18 +0700

    update

--- app.py
+++ app.py
@@ -3,15 +3,24 @@ from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__) # flask app instance
 
-# top page
+### top page ###
 @app.route('/', methods=['GET', 'POST'])
 def page_top():
     return render_template('toppage.html') # rendering "toppage.html"
 
-# second page
-@app.route('/second', methods=['GET', 'POST'])
+### second page ###
+@app.route('/2', methods=['GET', 'POST'])
 def page_second():
-    return render_template('secondpage.html') # rendering "top.html"
+	if request.method == 'GET':
+		return render_template('page2.html') # rendering "page2.html"
+	elif request.method == 'POST':
+		text = request.form['input_text'].upper() # get text from form -> uppercase
+		return render_template('page2.html', return_text=text) # re-rendering "page2.html" with variable "return_text"
+
+### third page ###
+@app.route('/3', methods=['GET', 'POST'])
+def page_third():
+    return render_template('page3.html') # rendering "page3.html"
 
 # length page - return character length of the word as JSON
 @app.route('/<word>', methods=['GET', 'POST'])
